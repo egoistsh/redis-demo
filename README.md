5c9d44f761d8bd9dcbfde64e2866bc7c2ca37dfa
billy <g_billy@163.com>
2022-07-31 23:51:47 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,14 @@
+# 项目简介
+redis-application 是一个用来展示 redis 各种应用场景的项目。
+
+目前按数据结构进行模块划分。
+
+- string
+- list
+- hash
+- set
+- sorted set
+- bitmap  
+- hyperloglog
+- geo
+- stream
---
+++ src/main/java/com/egoistsh/demo/Application.java
@@ -0,0 +1,11 @@
+package com.egoistsh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
